dd9f8737a9eaad5f559c2eb9f7d5c2d1767b2efb
Jordan269244 <wil22011@byui.edu>
2024-02-26 12:49:05 -0700

    Did Prep 1 & 2, James Bond program and Grade calulation.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?");
+        string userInput = Console.ReadLine();
+        int percent = int.Parse(userInput);
+
+        string letter = "";
+
+        if (percent >= 90 )
+        {
+            letter = "A";  
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F"; 
+        }
+
+        string symbol = "";
+
+        if ((percent % 10) >= 7)
+        {
+            symbol  = "+";
+        }
+        else if ((percent % 10) <=3)
+        {
+            symbol = "-";
+        }
+        else
+        {
+            symbol = "";
+        }
+
+
+        Console.WriteLine($"You have a {letter}{symbol} in the class");
+        if (letter == "A" || letter == "B" || letter == "C")
+        {
+            Console.Write("Congrats on passing the class!");
+        }
+        else
+        {
+            Console.Write("You aren't doing so hot, keep working at it!");
+        }
     }
 }
