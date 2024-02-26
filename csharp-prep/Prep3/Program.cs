a1483797c1354198483c609fd2f1d3de7e3a4c27
Jordan269244 <wil22011@byui.edu>
2024-02-26 16:13:11 -0700

    Prep assignment 3 magic number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.WriteLine("What is the magic number?");
+        //int magic = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1,101);
+
+        int guess = -1;
+
+        while (guess != magic)
+        {
+            Console.WriteLine("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+        
+            if (guess > magic)
+            {
+                Console.Write($"{guess} is too high, try a lower number");
+            }
+            else if (guess < magic)
+            {
+                Console.Write($"{guess} is too low, try a higher number.");
+            }
+            else
+            {
+                Console.Write($"YES! {magic} is the magic number!");
+            }
+        }
+
     }
 }
