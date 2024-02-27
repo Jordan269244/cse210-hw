caddf4ca9b5e4d7340044f41152251b1d38cc847
Jordan269244 <wil22011@byui.edu>
2024-02-27 13:41:34 -0700

    Finished Prep 5, was easy

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,59 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayMessage();
+
+        string Name = UserName();
+        int Number = FavoriteNumber();
+
+        int NewNumber = CalculateSquared(Number);
+
+        string NewSentence = DisplaySentence(NewNumber,Name);
+    
     }
+
+
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the program!");
+
+        }
+        
+        static string UserName()
+        {
+            Console.Write("What is your name?");
+            string InputName = Console.ReadLine();
+
+            return InputName;
+        }
+
+        static int FavoriteNumber()
+        {
+            Console.Write("What is your favorite number?");
+            int UserNumber = int.Parse(Console.ReadLine());
+
+            return UserNumber;
+        }
+
+        static int CalculateSquared(int Number)
+        {
+            int SquaredNumber = Number*Number;
+
+            return SquaredNumber;
+        }
+
+        static string DisplaySentence(int NewNumber, string Name)
+        {
+            Console.WriteLine($"{Name}, your favorite number squared is {NewNumber}.");
+            string Sentence = Console.ReadLine();
+
+            return Sentence;
+        }
+
+
+    
 }
