5b694b5241c68f3d64a0ff762af93e4be5c8e6f6
Jordan269244 <wil22011@byui.edu>
2024-02-27 13:13:08 -0700

    Number calculator, for some reason asks twice? Not sure why

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,56 @@
-using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of number, type 0 when finished");
+
+       
+        List<int> numbers = new List<int>();
+
+
+        int number = -1;
+
+        while(number != 0)
+        {   
+            Console.Write("Enter a number:");
+            string userInput = Console.ReadLine();
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+               numbers.Add(number); 
+            }
+        
+        }
+
+        int sum = 0;
+        foreach (int amount in numbers)
+        {
+            sum += amount;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+           int max = numbers[0];
+
+        foreach (int amount in numbers)
+        {
+            if (amount > max)
+            {
+                // if this number is greater than the max, we have found the new max!
+                max = amount;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
+    
+
     }
 }
