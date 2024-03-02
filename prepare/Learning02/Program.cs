6ab0af3a089b0aca2f9efc4d1f58ef51690cc81b
Jordan269244 <wil22011@byui.edu>
2024-03-02 13:49:47 -0700

    Finished Prepare, Resume Code

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Google";
+        job1._startYear = 1998;
+        job1._endYear = 2019;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Computer Scientist";
+        job2._company = "Microsoft";
+        job2._startYear = 2004;
+        job2._endYear = 2023;
+        
+        job1.Display();
+        job2.Display();
+
+
+        Resume myResume = new Resume();
+        myResume._name = "Jordan Wilson";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
+
+    
+
+        
 }
